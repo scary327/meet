bc864e1ed1bcd624586b33a4f705ab20022a3831
whiteduke <zef1rflis@yandex.ru>
2025-10-31 19:41:32 +0500

    microfixes

--- src/zvonilka/src/components/Chat/Chat.tsx
+++ src/zvonilka/src/components/Chat/Chat.tsx
@@ -6,11 +6,11 @@ import Button from "@shared/ui/Button/Button";
 
 export const Chat = memo(() => {
   return (
-    <div className="h-full flex flex-col items-center border-[1px] border-[var(--default-black)] rounded-xl">
+    <div className="h-[calc(100vh-var(--y-padding))] flex flex-col items-center border-[1px] border-[var(--default-black)] rounded-xl">
       <Typography.H1 className="!text-[32px] !font-medium !py-[12px] w-full border-b-[1px] border-b-[var(--default-black)] text-center rounded-xl">
         Чат
       </Typography.H1>
-      <div className="h-full min-h-[77vh]"></div>
+      <div className="h-full"></div>
       <div className="p-[12px] flex items-center gap-x-1 w-full border-t-[1px] border-t-[var(--default-black)] rounded-xl">
         <Input variant="chat" className="w-full" />
         <Button variant="secondary" shape="circle">
