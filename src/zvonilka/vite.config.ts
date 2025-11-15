fb3c50fbacc4cbb3c235e029ee320f628a0056c0
whiteduke <zef1rflis@yandex.ru>
2025-11-16 02:05:09 +0500

    Revert "feat: init docker for zvonilka"
    This reverts commit 2d1ca376db79a7e98861eced1e919f1945e117b0.

--- src/zvonilka/vite.config.ts
+++ src/zvonilka/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    host: true, // Listen on all addresses, needed for Docker
   },
   resolve: {
     alias: [
